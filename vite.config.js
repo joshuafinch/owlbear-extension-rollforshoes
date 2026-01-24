691ff067abb807e22ec8cfa9d92139b357c1c677
Joshua Finch <7941898+joshuafinch@users.noreply.github.com>
2026-01-24 18:35:26 +0000

    chore: update dependencies and configure Cloudflare plugin
    - Added @cloudflare/vite-plugin to devDependencies for Cloudflare integration.
    - Included cloudflare plugin in Vite configuration for enhanced deployment capabilities.
    - Introduced wrangler.jsonc for managing Cloudflare Workers configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite"
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from "@cloudflare/vite-plugin"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), cloudflare()],
   server: {
     https: {
       key: "./certs/localhost+2-key.pem",
